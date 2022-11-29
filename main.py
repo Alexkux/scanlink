854176e37bf2a360b2db0d2046ecc2a362df0737
Alexkux <kuznetsov-aa@ya.ru>
2022-11-29 09:26:17 +0400

    Входной файл links.txt на выходе links_for_scan.txt

--- main.py
+++ main.py
@@ -3,29 +3,39 @@
 К заданному шаблону добавляет номерацию страниц по возрастанию.
 Проводится проверка url на статус.
 """
-
+import csv
 import requests
+import re
+from bs4 import BeautifulSoup
+
 # Задаём шаблон url:
-#url = 'https://tachka.ru/kreplenie-dlya-velosipeda?page='
-#url = 'https://tachka.ru/farkop?page='
-url = 'https://tachka.ru/zashita-kartera?page='
-#url = 'https://tachka.ru/bagazhnik?page='
-#url='https://tachka.ru/bagazhnik/na-reilingi?page='
 
-# urls - массив ссылок
-urls = []
+url_collection = set()
 
-for i in range(1000):
-    url_new = url+str(i+1) # Присоединяем номер страницы к url
-    if requests.get(url_new).status_code == 200: # Проверка стр на доступность
-        print(url_new)  # не обязательный шаг
-        urls.append(url_new) # Добавляем существующий url в массив
-    else:
-        break
+urls_file='links.txt'
+with open(urls_file) as file:
+    url_collection = [row.strip() for row in file]
+
+url_collection = url_collection[1:]
+
+print(url_collection)
+urls = [] # urls - массив ссылок
+
+for link in url_collection:
+    url = link
+
+    for i in range(1000):
+        url_new = url+str(i+1) # Присоединяем номер страницы к url
+        if requests.get(url_new).status_code == 200: # Проверка стр на доступность
+            print(url_new)  # не обязательный шаг
+            urls.append(url_new) # Добавляем существующий url в массив
+        else:
+            break
 
 #print(urls) # вывод массива на экран
 k = url.rfind('?')
-out_file = (url[18:k] + '.txt').replace('/','-')
+#out_file = (url[18:k] + '.txt').replace('/','-')
+out_file
 print(out_file)
 
 with open(out_file, "w") as file: # Создаём файл для хранения ссылок
